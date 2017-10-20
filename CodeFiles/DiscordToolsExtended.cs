119ae9dbca9542ac540f28b40c41f4ef7f36db12
Devon <Sanitygamers@outlook.com>
2017-10-20 15:45:10 -0500

    Update DiscordToolsExtended.cs

--- CodeFiles/DiscordToolsExtended.cs
+++ CodeFiles/DiscordToolsExtended.cs
@@ -7,18 +7,30 @@ namespace DTE {
     * Use PluginDisable() to Unlink
     * Use pluginMain() to determine the main method
     */
-    string apiPath = @"C:/ProgramFiles/DiscordToolsExtended/Plugins"
-    string pluginname, author, mainpath;
-    int version;
+    string apiPath = @"C:/ProgramFiles/DiscordToolsExtended/Plugins";
+    ArrayList pluginname = new ArrayList();
+    ArrayList author = new ArrayList();
+    ArrayList mainpath = new ArrayList();
+    ArrayList version = new ArrayList();
+      
     
     
+    // Reloading 
+    
+    public void Disable() {
+      
+    }
+    
+    public void RemovePlugin() {
+      
+    }
     
     public void PluginInformation(string pluginname, string author, string mainpath, int version) {
-    
-    this.pluginname = pluginname;
-    this.author = author;
-    this.mainpath = mainpath;
-    this.version = version;
+     
+      pluginname.add(pluginname);
+      author.add(author);
+      mainpath.add(mainpath);
+      version.add(version)
     }
     
     public void consoleDisplay(string Message) {
